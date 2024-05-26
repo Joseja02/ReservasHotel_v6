fe55f7dc0a0a81840c1dbeed46a3ae05dcc69d3f
Jose Javier Sierra Berdun <jsieber1908@gmail.com>
2024-05-27 01:23:24 +0200

    Se añaden todos los archivos de ReservasHotel_v5

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/grafica/LanzadorVentanaPrincipal.java
@@ -0,0 +1,42 @@
+package org.iesalandalus.programacion.reservashotel.vista.grafica;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.iesalandalus.programacion.reservashotel.vista.grafica.recursos.LocalizadorRecursos;
+import org.iesalandalus.programacion.reservashotel.vista.grafica.utilidades.Dialogos;
+
+public class LanzadorVentanaPrincipal extends Application {
+
+    public void comenzar() throws Exception {
+        Stage escenario = new Stage();
+        start(escenario);
+    }
+    @Override
+    public void start(Stage escenarioPrincipal) {
+        try {
+            Parent raiz = FXMLLoader.load(LocalizadorRecursos.class.getResource("vistas/ventanaPrincipal.fxml"));
+            Scene escena = new Scene(raiz, 800, 600);
+            escenarioPrincipal.setTitle("Reservas Hotel v5 - Jose Javier Sierra Berdun");
+            escenarioPrincipal.setResizable(false);
+            escenarioPrincipal.setScene(escena);
+            escenarioPrincipal.setOnCloseRequest(e->confirmarSalida(escenarioPrincipal,e));
+            escenarioPrincipal.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void confirmarSalida(Stage escenarioPrincipal, WindowEvent e){
+        if (Dialogos.mostrarDialogoConfirmacion("Reservas Hotel v5 - Jose Javier Sierra Berdun", "Estas seguro que quieres salirte de la aplicacion"))
+        {
+            escenarioPrincipal.close();
+        }
+        else
+            e.consume();
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/texto/Opcion.java
@@ -0,0 +1,125 @@
+package org.iesalandalus.programacion.reservashotel.vista.texto;
+
+public enum Opcion {
+
+    SALIR("Salir") {
+        @Override
+        public void ejecutar() {
+            System.out.print("�Hasta luego! - Tarea Online 6 | Jose Javier Sierra Berd�n");
+        }
+    },
+    INSERTAR_HUESPED("Insertar Hu�sped") {
+        @Override
+        public  void ejecutar() {
+            vista.insertarHuesped();
+        }
+    },
+    BUSCAR_HUESPED("Buscar Hu�sped") {
+        @Override
+        public void ejecutar() {
+            vista.buscarHuesped();
+        }
+    },
+    BORRAR_HUESPED("Borrar Hu�sped") {
+        @Override
+        public void ejecutar() {
+            vista.borrarHuesped();
+        }
+    },
+    MOSTRAR_HUESPEDES("Mostar Hu�spedes"){
+        @Override
+        public void ejecutar() {
+            vista.mostrarHuespedes();
+        }
+    },
+    INSERTAR_HABITACION("Insertar Habitaci�n"){
+        @Override
+        public void ejecutar() {
+            vista.insertarHabitacion();
+        }
+    },
+    BUSCAR_HABITACION("Buscar Habitaci�n"){
+        @Override
+        public void ejecutar() {
+            vista.buscarHabitacion();
+        }
+    },
+    BORRAR_HABITACION("Borrar Habitaci�n"){
+        @Override
+        public void ejecutar() {
+            vista.borrarHabitacion();
+        }
+    },
+    MOSTRAR_HABITACIONES("Mostrar Habitaciones"){
+        @Override
+        public void ejecutar() {
+            vista.mostrarHabitaciones();
+        }
+    },
+    INSERTAR_RESERVA("Insertar Reserva"){
+        @Override
+        public void ejecutar() {
+            vista.insertarReserva();
+        }
+    },
+    ANULAR_RESERVA("Anular Reserva"){
+        @Override
+        public void ejecutar() {
+            vista.anularReserva();
+        }
+    },
+    MOSTRAR_RESERVAS("Mostrar Reservas"){
+        @Override
+        public void ejecutar() {
+            vista.mostrarReservas();
+        }
+    },
+    LISTAR_RESERVAS_HUESPED("Listar Reservas de un hu�sped"){
+        @Override
+        public void ejecutar() {
+            vista. mostrarReservasHuesped();
+        }
+    },
+    LISTAR_RESERVAS_TIPO_HABITACION("Listar Reservas de un tipo de habitaci�n"){
+        @Override
+        public void ejecutar() {
+            vista. mostrarReservasTipoHabitacion();
+        }
+    },
+    CONSULTAR_DISPONIBILIDAD("Consultar Disponibilidad"){
+        @Override
+        public void ejecutar() {
+            vista.comprobarDisponibilidad();
+        }
+    },
+    REALIZAR_CHECKIN("Realizar Checkin"){
+        @Override
+        public void ejecutar() {
+            vista.realizarCheckin();
+        }
+    },
+    REALIZAR_CHECKOUT("Realizar Checkout"){
+        @Override
+        public void ejecutar() {
+            vista.realizarCheckOut();
+        }
+    };
+    private String mensajeAMostrar;
+    private static VistaTexto vista;
+
+    public abstract void ejecutar();
+
+    static void setVista(VistaTexto vista) {
+        if (vista == null) {
+            throw new NullPointerException("ERROR: La vista no puede ser nula.");
+        }
+        Opcion.vista = vista;
+    }
+    private Opcion(String mensajeAMostrar) {
+        this.mensajeAMostrar = mensajeAMostrar;
+    }
+    @Override
+    public String toString() {
+        return String.format("%d.- %s", ordinal(), mensajeAMostrar);
+    }
+}
